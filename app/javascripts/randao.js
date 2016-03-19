88ef22c5ed5daf5e1c9e0de2803c5736416e845e
Aaaaaashu <orangehat.rb@gmail.com>
2016-03-19 15:39:41 +0800

    Add raodao.js and some third part lib

---
+++ app/javascripts/randao.js
@@ -0,0 +1,9 @@
+// var cb = function(res){console.log(res);}
+// Pudding.defaults({gas: 2000000});
+// web3.eth.defaultAccount = this.defaultAccount;
+import $ from './lib/jquery.min'
+import mouseTracker from './mouseTracker'
+
+$(() => {
+  mouseTracker.init()
+})
