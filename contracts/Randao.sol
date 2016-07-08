26fcf76c3be6929d2b45683f195537512e92db39
u2 <zhangyaning1985@gmail.com>
2016-07-08 10:48:24 +0800

    COP

--- contracts/Randao.sol
+++ contracts/Randao.sol
@@ -1,3 +1,4 @@
+// version 1.0
 contract Randao {
   struct Participant {
       uint256   secret;
@@ -15,14 +16,14 @@ contract Randao {
   struct Campaign {
       uint32    bnum;
       uint96    deposit;
-      uint8     commitBalkline;
-      uint8     commitDeadline;
+      uint16    commitBalkline;
+      uint16    commitDeadline;
 
-      uint16    revealsNum;
       uint256   random;
       bool      settled;
       uint256   bountypot;
       uint32    commitNum;
+      uint32    revealsNum;
 
       mapping (address => Consumer) consumers;
       mapping (address => Participant) participants;
@@ -33,14 +34,31 @@ contract Randao {
   address public founder;
 
   uint256 public bounty          = 1 ether;
-  uint8  public constant version = 1;
 
-  event CampaignAdded(uint256 campaignID, uint32 bnum, uint96 deposit, uint8 commitBalkline, uint8 commitDeadline, uint256 bountypot);
-  event Commit(uint256 CampaignId, address from, bytes32 commitment);
-  event Reveal(uint256 CampaignId, address from, uint256 secret);
-  event Follow(uint256 CampaignId, address from, uint256 bountypot);
+  // Prevents methods from perfoming any value transfer
+  modifier noEther() { if (msg.value > 0) throw; _}
 
-  modifier timeLineCheck(uint32 _bnum, uint8 _commitBalkline, uint8 _commitDeadline) {
+  modifier blankAddress(address _n) { if (_n != 0) throw; _}
+
+  modifier checkBounty { if (msg.value < bounty) throw; _}
+
+  modifier moreThanZero(uint256 _deposit) { if (_deposit <= 0) throw; _}
+
+  modifier notBeBlank(bytes32 _s) { if (_s == "") throw;  _}
+
+  modifier beBlank(bytes32 _s) { if (_s != "") throw; _}
+
+  modifier beFalse(bool _t) { if (_t) throw; _}
+
+  modifier beTrue(bool _t) { if (!_t) throw; _}
+
+  function Randao() {
+      founder = msg.sender;
+  }
+
+  event CampaignAdded(uint256 campaignID, uint32 bnum, uint96 deposit, uint16 commitBalkline, uint16 commitDeadline, uint256 bountypot);
+
+  modifier timeLineCheck(uint32 _bnum, uint16 _commitBalkline, uint16 _commitDeadline) {
       if (block.number >= _bnum) throw;
       if (_commitBalkline <= 0) throw;
       if (_commitDeadline <= 0) throw;
@@ -49,22 +67,11 @@ contract Randao {
       _
   }
 
-  // Prevents methods from perfoming any value transfer
-  modifier noEther() { if (msg.value > 0) throw; _}
-
-  modifier checkBounty { if (msg.value < bounty) throw; _}
-
-  modifier moreThanZero(uint256 _deposit) { if (_deposit <= 0) throw; _}
-
-  function Randao() {
-      founder = msg.sender;
-  }
-
   function newCampaign(
       uint32 _bnum,
       uint96 _deposit,
-      uint8 _commitBalkline,
-      uint8 _commitDeadline
+      uint16 _commitBalkline,
+      uint16 _commitDeadline
   ) timeLineCheck(_bnum, _commitBalkline, _commitDeadline)
     checkBounty moreThanZero(_deposit) external returns (uint256 _campaignID) {
       _campaignID = campaigns.length++;
@@ -79,55 +86,98 @@ contract Randao {
       CampaignAdded(_campaignID, _bnum, _deposit, _commitBalkline, _commitDeadline, msg.value);
   }
 
-  function follow(uint256 _campaignID) checkBounty external returns (bool) {
+  event Follow(uint256 CampaignId, address from, uint256 bountypot);
+
+  function follow(uint256 _campaignID)
+    checkBounty external returns (bool) {
       Campaign c = campaigns[_campaignID];
-      if (block.number < c.bnum - c.commitDeadline) {
-          Consumer consumer = c.consumers[msg.sender];
-          if (consumer.caddr != 0) throw;
-          c.bountypot += msg.value;
-          c.consumers[msg.sender] = Consumer(msg.sender, msg.value);
-          Follow(_campaignID, msg.sender, msg.value);
-          return true;
-      }
+      Consumer consumer = c.consumers[msg.sender];
+      return followCampaign(_campaignID, c, consumer);
   }
 
-  function commit(uint256 _campaignID, bytes32 _hs) external {
-      Campaign c = campaigns[_campaignID];
-      if (msg.value != c.deposit) throw;
-
-      if (block.number >= c.bnum - c.commitBalkline
-          && block.number < c.bnum - c.commitDeadline){
-          if (_hs != "" && c.participants[msg.sender].commitment == ""){
-              c.participants[msg.sender] = Participant(0, _hs, 0, false, false);
-              c.commitNum++;
-              Commit(_campaignID, msg.sender, _hs);
-          } else {
-              throw;
-          }
-      } else {
-          throw;
-      }
+  modifier checkFollowPhase(uint256 _bnum, uint16 _commitDeadline) {
+      if (block.number > _bnum - _commitDeadline) throw;
+      _
   }
 
+  function followCampaign(
+      uint256 _campaignID,
+      Campaign storage c,
+      Consumer storage consumer
+  ) checkFollowPhase(c.bnum, c.commitDeadline)
+    blankAddress(consumer.caddr) internal returns (bool) {
+      c.bountypot += msg.value;
+      c.consumers[msg.sender] = Consumer(msg.sender, msg.value);
+      Follow(_campaignID, msg.sender, msg.value);
+      return true;
+  }
+
+  event Commit(uint256 CampaignId, address from, bytes32 commitment);
+
+  function commit(uint256 _campaignID, bytes32 _hs) notBeBlank(_hs) external {
+      Campaign c = campaigns[_campaignID];
+      commitmentCampaign(_campaignID, _hs, c);
+  }
+
+  modifier checkDeposit(uint256 _deposit) { if (msg.value != _deposit) throw; _}
+
+  modifier checkCommitPhase(uint256 _bnum, uint16 _commitBalkline, uint16 _commitDeadline) {
+      if (block.number < _bnum - _commitBalkline) throw;
+      if (block.number > _bnum - _commitDeadline) throw;
+      _
+  }
+
+  function commitmentCampaign(
+      uint256 _campaignID,
+      bytes32 _hs,
+      Campaign storage c
+  ) checkDeposit(c.deposit)
+    checkCommitPhase(c.bnum, c.commitBalkline, c.commitDeadline)
+    beBlank(c.participants[msg.sender].commitment) internal {
+      c.participants[msg.sender] = Participant(0, _hs, 0, false, false);
+      c.commitNum++;
+      Commit(_campaignID, msg.sender, _hs);
+  }
+
+  // For test
   function getCommitment(uint256 _campaignID) noEther external returns (bytes32) {
       Campaign c = campaigns[_campaignID];
       Participant p = c.participants[msg.sender];
       return p.commitment;
   }
 
+  event Reveal(uint256 CampaignId, address from, uint256 secret);
+
   function reveal(uint256 _campaignID, uint256 _s) noEther external {
       Campaign c = campaigns[_campaignID];
-      if (block.number < c.bnum
-          && block.number >= c.bnum - c.commitDeadline) {
-          Participant p = c.participants[msg.sender];
-          if (sha3(_s) == p.commitment && !p.revealed) {
-              c.revealsNum++;
-              p.secret = _s;
-              p.revealed = true;
-              c.random ^= p.secret;
-              Reveal(_campaignID, msg.sender, _s);
-        }
-      }
+      Participant p = c.participants[msg.sender];
+      revealCampaign(_campaignID, _s, c, p);
+  }
+
+  modifier checkRevealPhase(uint256 _bnum, uint16 _commitDeadline) {
+      if (block.number <= _bnum - _commitDeadline) throw;
+      if (block.number >= _bnum) throw;
+      _
+  }
+
+  modifier checkSecret(uint256 _s, bytes32 _commitment) {
+      if (sha3(_s) == _commitment) throw;
+      _
+  }
+
+  function revealCampaign(
+    uint256 _campaignID,
+    uint256 _s,
+    Campaign storage c,
+    Participant storage p
+  ) checkRevealPhase(c.bnum, c.commitDeadline)
+    checkSecret(_s, p.commitment)
+    beFalse(p.revealed) internal {
+      p.secret = _s;
+      p.revealed = true;
+      c.revealsNum++;
+      c.random ^= p.secret;
+      Reveal(_campaignID, msg.sender, _s);
   }
 
   function getRandom(uint256 _campaignID) noEther external returns (uint256) {
@@ -140,52 +190,84 @@ contract Randao {
       }
   }
 
+  // The commiter get his bounty and deposit, there are three situations
+  // 1. Campaign succeeds.Every revealer gets his deposit and the bounty.
+  // 2. Someone revels, but some does not,Campaign fails.
+  // The revealer can get the deposit and the fines are distributed.
+  // 3. Nobody reveals, Campaign fails.Every commiter can get his deposit.
   function getMyBounty(uint256 _campaignID) noEther external {
       Campaign c = campaigns[_campaignID];
-      if (c.settled == true) {
-          Participant p = c.participants[msg.sender];
-          if (!p.rewarded) {
-              if (p.revealed) {
-                  uint256 share;
-                  if (c.commitNum > c.revealsNum) {
-                      share = fines(c) / c.revealsNum;
-                  } else {
-                      share = c.bountypot / c.revealsNum;
-                  }
-                  transferBounty(c, p, share);
-              } else {
-                  if (c.revealsNum == 0) {
-                      transferBounty(c, p, 0);
-                  }
-              }
+      Participant p = c.participants[msg.sender];
+      transferBounty(c, p);
+  }
+
+  function transferBounty(
+      Campaign storage c,
+      Participant storage p
+    ) beTrue(c.settled)
+      beFalse(p.rewarded) internal {
+      if (c.revealsNum > 0) {
+          if (p.revealed) {
+              uint256 share = calculateShare(c);
+              returnReward(share, c, p);
           }
+      // Nobody reveals
+      } else {
+          returnReward(0, c, p);
       }
   }
 
-  function transferBounty(Campaign _c, Participant _p, uint256 _share) internal {
-      _p.reward = _share;
-      _p.rewarded = true;
-      if (!msg.sender.send(_share + _c.deposit)) {
-          _p.reward = 0;
-          _p.rewarded = false;
+  function calculateShare(Campaign c) internal returns (uint256 _share) {
+      // Someone does not reveal. Campaign fails.
+      if (c.commitNum > c.revealsNum) {
+          _share = fines(c) / c.revealsNum;
+      // Campaign succeeds.
+      } else {
+          _share = c.bountypot / c.revealsNum;
       }
   }
 
-  function fines(Campaign _c) internal returns (uint256) {
-      return (_c.commitNum - _c.revealsNum) * _c.deposit;
+  function returnReward(
+      uint256 _share,
+      Campaign storage c,
+      Participant storage p
+  ) internal {
+      p.reward = _share;
+      p.rewarded = true;
+      if (!msg.sender.send(_share + c.deposit)) {
+          p.reward = 0;
+          p.rewarded = false;
+      }
+  }
+
+  function fines(Campaign c) internal returns (uint256) {
+      return (c.commitNum - c.revealsNum) * c.deposit;
   }
 
   function refundBounty(uint256 _campaignID) noEther external {
       Campaign c = campaigns[_campaignID];
-      if (c.settled
-          && c.consumers[msg.sender].caddr != msg.sender
-          && c.revealsNum < c.commitNum
-          && c.consumers[msg.sender].bountypot > 0) {
-          uint256 bountypot = c.consumers[msg.sender].bountypot;
-          c.consumers[msg.sender].bountypot = 0;
-          if (!msg.sender.send(bountypot)) {
-              c.consumers[msg.sender].bountypot = bountypot;
-          }
+      returnBounty(_campaignID, c);
+  }
+
+  modifier campaignFailed(uint32 _commitNum, uint32 _revealsNum) {
+      if (_commitNum > _revealsNum) throw;
+      _
+  }
+
+  modifier beConsumer(address _caddr) {
+      if (_caddr != msg.sender) throw;
+      _
+  }
+
+  // If the campaign fails, the consumers can get back the bounty.
+  function returnBounty(uint256 _campaignID, Campaign storage c)
+    beTrue(c.settled)
+    campaignFailed(c.commitNum, c.revealsNum)
+    beConsumer(c.consumers[msg.sender].caddr) internal {
+      uint256 bountypot = c.consumers[msg.sender].bountypot;
+      c.consumers[msg.sender].bountypot = 0;
+      if (!msg.sender.send(bountypot)) {
+          c.consumers[msg.sender].bountypot = bountypot;
       }
   }
 }
