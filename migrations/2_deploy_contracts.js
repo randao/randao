60d4862dd0d4a25d08ac6d89022cfd868a16d10e
Chris Hatch <chrishatch@cardano-labo.com>
2016-09-27 11:36:04 +0700

    remove unused truffle example contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,4 @@
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.autolink();
-  deployer.deploy(MetaCoin);
   deployer.deploy(Randao);
   deployer.deploy(Counter);
 };
