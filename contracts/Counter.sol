37e6d3987bcd0f3849f6cb105a07cfb065316a93
Chris Hatch <chrishatch@cardano-labo.com>
2016-10-26 13:37:04 +0700

    complete upgrade to Solidity 0.4 - now works with Truffle v2.1.0

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.3;
 
 contract Counter {
   uint public counter = 0;
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.3;
 
 contract Migrations {
   address public owner;
