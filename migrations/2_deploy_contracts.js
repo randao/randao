c3b452198ea46169a6610c161750c535c43240eb
Thomas Hodges <me@thodges.com>
2019-03-25 05:57:25 -0500

    Remove Counter and add newline to helper

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const Counter = artifacts.require("Counter");
 const Randao = artifacts.require("Randao");
 
 module.exports = function(deployer) {
-  deployer.deploy(Counter);
   deployer.deploy(Randao);
 };
